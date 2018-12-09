c885943161ae3c364db2e2cef9784485820cd7a4
caroljunq <junq.carolina@gmail.com>
2018-12-09 07:09:19 -0200

    mais correlacao

---
+++ corr_var_internacoes.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df_internacao = pd.read_csv('AutIntHos-DataSUS-2013-2018.csv',sep=";")
+
+
+# Normalização Cidade - Inteiro (indexacao municipio)
+cnes_municipio = df_internacao['CNES_MUNICIPIO']
+# Valore únicos
+cnes_municipio = set(cnes_municipio)
+
+cnes_municipio = list(cnes_municipio)
+
+indexacao_municipio = [(index, cnes_municipio[index]) for index in range(len(cnes_municipio))]
+
+dicio = dict((el, i) for i, el in indexacao_municipio)
+
+df_internacao['CNES_MUNICIPIO'] = df_internacao['CNES_MUNICIPIO'].replace(dicio)
+
+
+# Correlação de variáveis de internações
+# Complexidade e Valor Total
+# Há entre 64% e 77% de relação entre a complexidade da doença/atendimento paciente e o custo total
+print(df_internacao['COMPLEXIDADE'].corr(df_internacao['VALOR_TOTAL'],method='pearson'))
+print(df_internacao['COMPLEXIDADE'].corr(df_internacao['VALOR_TOTAL'],method='kendall'))
+print(df_internacao['COMPLEXIDADE'].corr(df_internacao['VALOR_TOTAL'],method='spearman'))
+
+# Valor total e total de dias (diaria)
+# Todas as correlações são negativas, portanto, não há corelação entre número de dias de internação e valor total cobrado
+print(df_internacao['DIARIA'].corr(df_internacao['VALOR_TOTAL'],method='pearson'))
+print(df_internacao['DIARIA'].corr(df_internacao['VALOR_TOTAL'],method='kendall'))
+print(df_internacao['DIARIA'].corr(df_internacao['VALOR_TOTAL'],method='spearman'))
+
+# Correlação entre cidade e tempo de internação
+print(df_internacao['DIARIA'].corr(df_internacao['CNES_MUNICIPIO'],method='pearson'))
+print(df_internacao['DIARIA'].corr(df_internacao['CNES_MUNICIPIO'],method='kendall'))
+print(df_internacao['DIARIA'].corr(df_internacao['CNES_MUNICIPIO'],method='spearman'))
+
+# Correlação entre valor total e valor de uti
+print(df_internacao['VALOR_TOTAL'].corr(df_internacao['VALOR_UTI'],method='pearson'))
+print(df_internacao['VALOR_TOTAL'].corr(df_internacao['VALOR_UTI'],method='kendall'))
+print(df_internacao['VALOR_TOTAL'].corr(df_internacao['VALOR_UTI'],method='spearman'))
--- itau_regression.py
+++ itau_regression.py
@@ -1,7 +1,3 @@
-import pandas as pd
-# from sklearn.neighbors import KNeighborsRegressor
-#
-# df_internacao = pd.read_csv('AutIntHos-DataSUS-2013-2018.csv',sep=";")
 df_obitos = pd.read_csv('Obitos-DataSUS-2013-2016.csv',sep=";")
 
 #boston = datasets.load_boston()
@@ -13,9 +9,9 @@ df_obitos = pd.read_csv('Obitos-DataSUS-2013-2016.csv',sep=";")
 #
 # outputs = knn.predict(x )
 
-obito_municipio = df_obitos['OBITO_MUNICIPIO']
-obito_municipio = set(obito_municipio)
-obito_municipio = list(obito_municipio)
-indexacao_municipio = [(index, obito_municipio[index]) for index in range(len(obito_municipio))]
-dicio = dict((el, i) for i, el in indexacao_municipio)
-df_obitos['OBITO_MUNICIPIO'] = df_obitos['OBITO_MUNICIPIO'].replace(dicio)
+# obito_municipio = df_obitos['OBITO_MUNICIPIO']
+# obito_municipio = set(obito_municipio)
+# obito_municipio = list(obito_municipio)
+# indexacao_municipio = [(index, obito_municipio[index]) for index in range(len(obito_municipio))]
+# dicio = dict((el, i) for i, el in indexacao_municipio)
+# df_obitos['OBITO_MUNICIPIO'] = df_obitos['OBITO_MUNICIPIO'].replace(dicio)

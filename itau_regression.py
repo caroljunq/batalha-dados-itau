75eeeb8fb28411b7986562b44e3a62aeb41454c7
caroljunq <junq.carolina@gmail.com>
2018-12-09 05:54:04 -0200

    jenny

--- itau_regression.py
+++ itau_regression.py
@@ -7,20 +7,10 @@ header = df.iloc[0]
 df_obitos = df_obitos[1:]
 df.columns = header
 
-#
-#
-# boston = datasets.load_boston()
-# x,y = boston.data, boston.target
-#
-# knn = KNeighborsRegressor(n_neighbors=11,p=2)
-#
-# knn.fit(x,y)
-#
-# outputs = knn.predict(x )
 
-obito_municio = df_obitos['OBITO_MUNICIPIO']
-obito_municio = set(obito_municio)
-obito_municio = list(obito_municio)
-indexacao_municipio = [(index, obito_municio[index]) for index in range(len(obito_municio))]
-dicio = dict((el, i) for i, el in obito_municio)
-df['OBITO_MUNICIPIO'] = df['OBITO_MUNICIPIO'].replace(dicio)
+# obito_municio = df_obitos['OBITO_MUNICIPIO']
+# obito_municio = set(obito_municio)
+# obito_municio = list(obito_municio)
+# indexacao_municipio = [(index, obito_municio[index]) for index in range(len(obito_municio))]
+# dicio = dict((el, i) for i, el in obito_municio)
+# df['OBITO_MUNICIPIO'] = df['OBITO_MUNICIPIO'].replace(dicio)

18a7377a067357457673b23aa5c5d5d1196f63eb
caroljunq <junq.carolina@gmail.com>
2018-12-09 02:27:48 -0200

    dados

---
+++ itau_regression.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# df_internacao = pd.read_csv('AutIntHos-DataSUS-2013-2018.csv',sep=";")
+df_estabelecimentos = pd.read_csv('neoway_estabelecimentos_saude.csv')
+# df_obitos = pd.read_csv('Obitos-DataSUS-2013-2016.csv',sep=";")
